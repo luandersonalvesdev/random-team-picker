4e6c3083200b8ad1222e4a6ed9dc53ab58a6bf26
Luanderson Alves <luaoderson@gmail.com>
2024-02-04 21:30:08 -0300

    refactor: login and signup page to mobile

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -5,7 +5,7 @@ export default function Login() {
 
   return (
     <main className='flex flex-col justify-center w-screen h-screen items-center'>
-      <div className='flex flex-col justify-center items-center bg-white rounded-lg py-6 px-8 shadow-md min-w-96 max-w-64'>
+      <div className='flex flex-col justify-center items-center bg-white rounded-lg py-6 px-8 shadow-md'>
         <LoginForm />
         <JoinAsGuest />
       </div>
--- frontend/src/pages/Signup/index.jsx
+++ frontend/src/pages/Signup/index.jsx
@@ -3,8 +3,8 @@ import JoinAsGuest from '../../components/JoinAsGuest';
 
 export default function Signup() {
   return (
-    <main className='flex flex-col justify-center h-screen items-center'>
-      <div className='flex flex-col justify-center items-center bg-white rounded-lg py-6 px-11 shadow-md min-w-96 max-w-64'>
+    <main className='flex flex-col justify-center w-screen h-screen items-center px-4'>
+      <div className='flex flex-col justify-center items-center bg-white rounded-lg py-6 px-8 shadow-md'>
         <SignupForm />
         <JoinAsGuest />
       </div>
