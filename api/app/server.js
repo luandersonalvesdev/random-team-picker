c7c1f323f53ae7b0db031917ddb0059347c1c8da
Luanderson Alves <luaoderson@gmail.com>
2024-02-04 21:58:20 -0300

    refactor: small things

--- api/app/server.js
+++ api/app/server.js
@@ -2,4 +2,4 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`"Team Drawer" running on port ${PORT}`));
+app.listen(PORT, () => console.log(`"Random Team Picker" running on port ${PORT}`));
